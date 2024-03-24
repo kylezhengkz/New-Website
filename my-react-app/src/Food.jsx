c91ea2b19da37ebb0fc2522c4f1a213769599c55
Kyle <kylezhengkz@gmail.com>
2024-03-24 16:46:07 -0400

    a

---
+++ my-react-app/src/Food.jsx
@@ -0,0 +1,14 @@
+
+function Food() {
+    const food1 = "Orange";
+    const food2 = "wet bananas";
+    
+    return (
+        <ul>
+            <li>{food1}</li>
+            <li>{food2.toUpperCase()}</li>
+        </ul>
+    );
+}
+
+export default Food
